f76cf3da31e1d96828f6bcf2e7cf46c93699e673
CoolStar <coolstarorganization@gmail.com>
2020-06-12 14:09:41 -0700

    fix pinning (2)

--- chimera1n-deploy-linux-macos.sh
+++ chimera1n-deploy-linux-macos.sh
@@ -83,11 +83,11 @@ echo 'echo "URIs: https://repo.theodyssey.dev/" >> /etc/apt/sources.list.d/odyss
 echo 'echo "Suites: ./" >> /etc/apt/sources.list.d/odyssey.sources' >> odyssey-device-deploy.sh
 echo 'echo "Components: " >> /etc/apt/sources.list.d/odyssey.sources' >> odyssey-device-deploy.sh
 echo 'echo "" >> /etc/apt/sources.list.d/odyssey.sources' >> odyssey-device-deploy.sh
-echo 'mkdir -p /etc/apt/preferenced.d/' >> odyssey-device-deploy.sh
-echo 'echo "Package: *" > /etc/apt/preferenced.d/odyssey' >> odyssey-device-deploy.sh
-echo 'echo "Pin: release n=odyssey-ios" >> /etc/apt/preferenced.d/odyssey' >> odyssey-device-deploy.sh
-echo 'echo "Pin-Priority: 1001" >> /etc/apt/preferenced.d/odyssey' >> odyssey-device-deploy.sh
-echo 'echo "" >> /etc/apt/preferenced.d/odyssey' >> odyssey-device-deploy.sh
+echo 'mkdir -p /etc/apt/preferences.d/' >> odyssey-device-deploy.sh
+echo 'echo "Package: *" > /etc/apt/preferences.d/odyssey' >> odyssey-device-deploy.sh
+echo 'echo "Pin: release n=odyssey-ios" >> /etc/apt/preferences.d/odyssey' >> odyssey-device-deploy.sh
+echo 'echo "Pin-Priority: 1001" >> /etc/apt/preferences.d/odyssey' >> odyssey-device-deploy.sh
+echo 'echo "" >> /etc/apt/preferences.d/odyssey' >> odyssey-device-deploy.sh
 echo 'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/X11:/usr/games dpkg -i org.coolstar.sileo_1.8.1_iphoneos-arm.deb' >> odyssey-device-deploy.sh
 echo 'uicache -p /Applications/Sileo.app' >> odyssey-device-deploy.sh
 echo 'echo -n "" > /var/lib/dpkg/available' >> odyssey-device-deploy.sh
