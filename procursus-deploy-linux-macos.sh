3bfe38a3616d508cf12a2cb7de46edf601da8118
Deon <bianjiahang@outlook.com>
2022-03-01 23:28:49 +0000

    Upload new straps and Sileo 2.3 (#45)
    * Upload Sileo 2.2.4-1
    
    I am faster than Amy!!!
    
    * Update new bootstraps and Sileo
    
    The new bootstraps will be updated when finished the testing
    
    * Upload new straps and Sileo 2.3
    
    * Upload new straps and Sileo 2.3
    
    * Don't have to run firmware
    
    * Only run firmware on iOS 12
    
    * Don't run the firmware wrapper
    
    Co-authored-by: m1stadev <adamhamdi31@gmail.com>

--- procursus-deploy-linux-macos.sh
+++ procursus-deploy-linux-macos.sh
@@ -15,7 +15,7 @@ ODYSSEYDIR=$(mktemp -d)
 
 cat << "EOF"
 Odysseyra1n Installation Script
-Copyright (C) 2021, CoolStar. All Rights Reserved
+Copyright (C) 2022, CoolStar. All Rights Reserved
 
 Before you begin:
 If you're currently jailbroken with a different bootstrap
@@ -73,12 +73,15 @@ SNAPSHOT=$(snappy -s | cut -d ' ' -f 3 | tr -d '\n')
 
 snappy -f / -r "$SNAPSHOT" -t orig-fs > /dev/null 2>&1
 /prep_bootstrap.sh
+if [[ $CFVER = "1500" ]]; then
+	/usr/libexec/firmware
+fi
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/X11:/usr/games
 if [[ $VER = 12.1* ]] || [[ $VER = 12.0* ]]; then
     dpkg -i org.swift.libswift_5.0-electra2_iphoneos-arm.deb > /dev/null
 fi
 echo "(4) Installing Sileo and upgrading Procursus packages..."
-dpkg -i org.coolstar.sileo_2.2.3_iphoneos-arm.deb > /dev/null
+dpkg -i org.coolstar.sileo_2.3_iphoneos-arm.deb > /dev/null
 uicache -p /Applications/Sileo.app
 mkdir -p /etc/apt/sources.list.d /etc/apt/preferences.d
 {
@@ -103,14 +106,14 @@ IPROXY=$(iproxy 28605 44 >/dev/null 2>&1 & echo $!)
 curl -sLOOOOO https://github.com/coolstar/Odyssey-bootstrap/raw/master/bootstrap_1500.tar.gz \
 	https://github.com/coolstar/Odyssey-bootstrap/raw/master/bootstrap_1600.tar.gz \
 	https://github.com/coolstar/Odyssey-bootstrap/raw/master/bootstrap_1700.tar.gz \
-	https://github.com/coolstar/Odyssey-bootstrap/raw/master/org.coolstar.sileo_2.2.3_iphoneos-arm.deb \
+	https://github.com/coolstar/Odyssey-bootstrap/raw/master/org.coolstar.sileo_2.3_iphoneos-arm.deb \
 	https://github.com/coolstar/Odyssey-bootstrap/raw/master/org.swift.libswift_5.0-electra2_iphoneos-arm.deb
 if [ ! "${ARM}" = yes ]; then
 	echo "(2) Copying resources to your device..."
 	echo "Default password is: alpine"
 	scp -qP28605 -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" bootstrap_1500.tar.gz \
 		bootstrap_1600.tar.gz bootstrap_1700.tar.gz \
-		org.coolstar.sileo_2.2.3_iphoneos-arm.deb \
+		org.coolstar.sileo_2.3_iphoneos-arm.deb \
 		org.swift.libswift_5.0-electra2_iphoneos-arm.deb \
 		odysseyra1n-install.bash \
 		root@127.0.0.1:/var/root/
